ff6460b0e72664a9491ac17bdaec2f2260352bf7
Jakub Darul <daruljakub@protonmail.com>
2020-02-29 14:10:20 +0100

    version bump

--- main-backend/src/main/scala/com/guys/coding/hackathon/backend/api/graphql/Mutation.scala
+++ main-backend/src/main/scala/com/guys/coding/hackathon/backend/api/graphql/Mutation.scala
@@ -3,12 +3,16 @@ package com.guys.coding.hackathon.backend.api.graphql
 import com.guys.coding.hackathon.backend.Services
 import com.guys.coding.hackathon.backend.api.graphql.schema.MutationHolder
 import sangria.schema.ObjectType
+import com.guys.coding.hackathon.backend.api.graphql.schema.example.ExampleMutation
+import com.guys.coding.hackathon.backend.domain.ExampleService
+import cats.effect.IO
 
 class Mutation(services: Services) {
 
   private val mutationHolders =
     List[MutationHolder](
-      )
+      new ExampleMutation(new ExampleService[IO]{})
+    )
 
   val MutationType = ObjectType(
     name = "Mutation",
