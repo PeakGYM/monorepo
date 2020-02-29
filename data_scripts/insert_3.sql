06110e68723e6ca5a730c0ad3c7b05cf54edd4f4
Ro6ertWcislo <robert.wcislo1995@gmail.com>
2020-02-29 23:40:12 +0100

    exercises sql script

---
+++ data_scripts/insert_3.sql
@@ -0,0 +1,5 @@
+insert into exercises(id, imgurl, name) values ('0', 'https://i.ibb.co/8sP3Hb2/squat.png','squat');
+insert into exercises(id, imgurl, name) values ('1', 'https://i.ibb.co/sKnydTQ/pull.jpg','pull');
+insert into exercises(id, imgurl, name) values ('2', 'https://i.ibb.co/8KNvVjz/pullups.jpg','pullups');
+insert into exercises(id, imgurl, name) values ('3', 'https://i.ibb.co/rc3jHCh/benchpress.jpg','benchpress');
+insert into exercises(id, imgurl, name) values ('4', 'https://i.ibb.co/PwZVrLC/bicep-curl.jpg','bicep curl');
