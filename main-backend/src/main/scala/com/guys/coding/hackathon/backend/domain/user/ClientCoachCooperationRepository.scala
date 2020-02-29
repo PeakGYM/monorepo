990aea29481dc17a824689908a8d41ffa95f5483
Ro6ertWcislo <robert.wcislo1995@gmail.com>
2020-02-29 17:47:13 +0100

    added client coach cooperation repository

---
+++ main-backend/src/main/scala/com/guys/coding/hackathon/backend/domain/user/ClientCoachCooperationRepository.scala
@@ -0,0 +1,13 @@
+package com.guys.coding.hackathon.backend.domain.user
+
+import com.guys.coding.hackathon.backend.domain.UserId.{ClientId, CoachId}
+import simulacrum.typeclass
+
+
+
+@typeclass
+trait ClientCoachCooperationRepository[F[_]] {
+  def insert(cooperation: ClientCoachCooperation): F[ClientCoachCooperation]
+  def getClientsForCoach(coach: Coach): F[List[ClientId]]
+  def getCoachesForClient(client: Client): F[List[CoachId]]
+}
--- main-backend/src/main/scala/com/guys/coding/hackathon/backend/domain/user/CoachRepository.scala
+++ main-backend/src/main/scala/com/guys/coding/hackathon/backend/domain/user/CoachRepository.scala
@@ -1,8 +1,10 @@
 package com.guys.coding.hackathon.backend.domain.user
 
+import com.guys.coding.hackathon.backend.domain.UserId.CoachId
 import simulacrum.typeclass
 
 @typeclass
 trait CoachRepository[F[_]] {
   def insert(coach: Coach): F[Coach]
+  def getCoachesById(ids: List[CoachId]): F[List[Coach]]
 }
