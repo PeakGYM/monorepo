8b09fa3a4a666f5d4c25ed7bd994d1a2a9f6fbfd
Łukasz Kaźmierczak <luki.pol@gmail.com>
2020-02-29 22:15:33 +0100

    work on search

--- frontend/src/map.js
+++ frontend/src/map.js
@@ -1,44 +1,55 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import React from "react";
+import { render } from "react-dom";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-const position = [50.0265321, 19.9489974]
+const position = [50.0265321, 19.9489974];
 
 export function loadMap(gyms) {
-
   let clientIcon = new L.Icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconUrl:
+      "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
   });
 
+  let formatCoachCount = coachIds => {
+    let count = coachIds.length;
 
-  let gymMarkers =
-      gyms.map(gym => {
-        let pos = [gym.location.lat, gym.location.lng];
+    return count === 1 ? "1 coach" : `{count} coaches`;
+  };
 
-        return (
-          <Marker riseOnHover={true} position={pos}>
-            <Popup>{gym.name}</Popup>
-          </Marker>
-        );
-      });
+  let gymMarkers = gyms.map(gym => {
+    let pos = [gym.location.lat, gym.location.lng];
 
+    return (
+      <Marker riseOnHover={true} position={pos}>
+        <Popup>
+          {gym.name} - {formatCoachCount(gym.coachIds)}
+        </Popup>
+      </Marker>
+    );
+  });
 
-  let map =
-    <Map center={position} zoom={16}>
+  let map = (
+    <Map center={position} zoom={16} minZoom={10} maxZoom={17}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <Marker icon={clientIcon} position={position}>
         <Popup>Your location</Popup>
       </Marker>
       {gymMarkers}
-    </Map>;
+    </Map>
+  );
 
-  render(map, document.getElementById('map-container'));
-};
+  render(map, document.getElementById("map-container"));
+}
+
+export function windowHeight() {
+  return window.innerHeight;
+}
