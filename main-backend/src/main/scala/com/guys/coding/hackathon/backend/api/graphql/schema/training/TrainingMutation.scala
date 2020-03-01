a9b6b57bf2c99a34d045e181dec257b0f083d763
Jakub Darul <daruljakub@protonmail.com>
2020-03-01 08:40:27 +0100

    added test endpoimt

--- main-backend/src/main/scala/com/guys/coding/hackathon/backend/api/graphql/schema/training/TrainingMutation.scala
+++ main-backend/src/main/scala/com/guys/coding/hackathon/backend/api/graphql/schema/training/TrainingMutation.scala
@@ -4,10 +4,20 @@ import com.guys.coding.hackathon.backend.api.graphql.service.GraphqlSecureContex
 import sangria.schema._
 import com.guys.coding.hackathon.backend.Services
 import com.guys.coding.hackathon.backend.api.graphql.schema.MutationHolder
+import com.guys.coding.hackathon.backend.domain.training.Training
+import com.guys.coding.hackathon.backend.infrastructure.slick.training.TrainingSchema.MuscleGroup
+import com.guys.coding.hackathon.backend.domain.UserId.CoachId
+import com.guys.coding.hackathon.backend.domain.UserId.ClientId
+import hero.common.util.time.TimeUtils
+import hero.common.util.IdProvider
+import com.guys.coding.hackathon.backend.infrastructure.Faker
 
 class TrainingMutation(services: Services) extends MutationHolder {
 
   val TrainingArg = Argument("training", TrainingInputTypes.TrainingInputType)
+  val FromArg     = Argument("from", LongType)
+  val CoachIdArg  = Argument("coachId", OptionInputType(StringType))
+  val ClientIdArg = Argument("clientId", StringType)
 
   override def mutationFields(): List[Field[GraphqlSecureContext, Unit]] =
     fields[GraphqlSecureContext, Unit](
@@ -31,6 +41,34 @@ class TrainingMutation(services: Services) extends MutationHolder {
         arguments = List(TrainingArg),
         resolve = c =>
           c.ctx.authorizedF { _ =>
+            // Should be taken from token
+            services.trainingRepository
+              .updateTraiing(c.arg(TrainingArg).toDomain)
+              .unsafeToFuture()
+
+          }
+      ),
+      Field(
+        "addTestWorkout",
+        OptionType(TrainingOutputTypes.TrainingType),
+        arguments = List(FromArg, CoachIdArg,ClientIdArg), //bcm
+        resolve = c =>
+          c.ctx.authorizedF { _ =>
+            val from = TimeUtils.millisToZonedDateTime(c.arg(FromArg))
+
+            val id = IdProvider.id.newId()
+
+            Training(
+              id =  id,
+              name = "BestTraining",
+              muscleGroup = MuscleGroup.Back :: Nil,
+              coachId = c.arg(CoachIdArg).map(CoachId),
+              clientId = ClientId(c.arg(ClientIdArg)),
+              dateFrom = from,
+              dateTo = from.plusHours(1),
+              exercises = (1 to 3).map(_ => Faker.plannedEx(id)).toList,
+              inperson = true
+            )
             // Should be taken from token
             services.trainingRepository
               .updateTraiing(c.arg(TrainingArg).toDomain)
